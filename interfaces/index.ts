b6ab0c5a22d8c1382b940351adf7e43a327baf15
Manuel <antoniomanu533@gmail.com>
2023-06-25 16:48:20 +0200

    Add favorite model

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,4 @@
+export { I_Favorite } from './favorite.interface';
 export { I_Gender } from './gender.interface';
 export { I_User } from './user.interface';
 export { I_Role } from './role.interface';
