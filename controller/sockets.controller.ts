642489c631c0f2bfb4495493727bea1d48dd10f2
Manuel <antoniomanu533@gmail.com>
2023-07-05 21:10:42 +0200

    change name ArraySokets

--- controller/sockets.controller.ts
+++ controller/sockets.controller.ts
@@ -1,16 +1,16 @@
 import { Server } from 'socket.io';
 import { CustomSocket } from '../interfaces';
 
-export let userAvtives: Array<{ userId: string, socketId: string }> = [];
+export let userActives: Array<{ userId: string, socketId: string }> = [];
 
 export const socketsController = (socket: CustomSocket, io: Server) => {
 
     const userID = socket.handshake.query.userId as string;
     socket.userId = userID;
-    userAvtives.push({ socketId: socket.id, userId: userID });
+    userActives.push({ socketId: socket.id, userId: userID });
 
     socket.on('disconnect', () => {
-        userAvtives = userAvtives.filter((s => s.socketId !== socket.id));
+        userActives = userActives.filter((s => s.socketId !== socket.id));
     });
 
 }
--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
 import { Server } from 'socket.io';
-import { userAvtives } from './sockets.controller';
+import { userActives } from './sockets.controller';
 
 
 export const register = async (req: Request, res: Response, io: Server) => {
-    const userSocektId = userAvtives.filter(obj => obj.userId === '12345').map(obj => obj.socketId);
-    console.log(userAvtives);
-
+    const userSocektId = userActives.filter(obj => obj.userId === '12345').map(obj => obj.socketId);
     if (userSocektId) {
         io.to(userSocektId).emit('prueba', { a: 'hola' });
     }
