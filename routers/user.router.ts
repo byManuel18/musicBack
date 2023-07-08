2af11e2c06e5537e2bb340eaef9c65d5f3fe7e7f
Manuel <antoniomanu533@gmail.com>
2023-07-08 17:50:08 +0200

    create Login method

--- routers/user.router.ts
+++ routers/user.router.ts
@@ -3,22 +3,31 @@ import { Server } from 'socket.io';
 import { UserController } from "../controller";
 import { DBValidators } from "../middlewares/inde";
 import { check } from "express-validator";
+import { I_UserRequest } from "../interfaces";
 
 
 const UserRouters = {
-    Register: '/register'
+    Register: '/register',
+    Login: '/login',
 } as const;
 
 
 DBValidators
 export const getUserRouter = (io: Server) => {
     const router: Router = Router();
+
     router.post(UserRouters.Register, [
         check('userName').custom(DBValidators.userNameValidator),
         check('email').custom(DBValidators.emailValidator),
         check('passWord').custom(DBValidators.passWordValidator),
         DBValidators.validarCampos
     ], (req: Request, res: Response) => { return UserController.register(req, res) });
+
+    router.post(UserRouters.Login, [
+        check('userNameOrEmail').custom(DBValidators.userNameOrEmailValidator),
+        check('passWord').custom(DBValidators.passWordValidator),
+        DBValidators.validarCampos
+    ], (req: Request, res: Response) => { return UserController.login(req as I_UserRequest, res) });
 
     return router;
 }
